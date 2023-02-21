21c743cab7553b53f93dd634713b5efadd133f4a
frank djenou <djenoufrank@gmail.com>
2023-02-21 19:21:32 +0100

    login refactorer

--- RYDX/app/src/main/java/com/example/rydx/screens/login/RYDXLoginScreen.kt
+++ RYDX/app/src/main/java/com/example/rydx/screens/login/RYDXLoginScreen.kt
@@ -29,6 +29,9 @@ import com.google.firebase.FirebaseException
 import com.google.firebase.auth.*
 import java.util.concurrent.TimeUnit
 
+val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
+
 @Preview
 @Composable
 fun  RYDXLoginScreen(navController: NavController = NavController(context = LocalContext.current)){
@@ -41,157 +44,120 @@ fun  RYDXLoginScreen(navController: NavController = NavController(context = Loca
     val otp = remember {
         mutableStateOf("")
     }
-
     val verificationID = remember {
         mutableStateOf("")
     }
-
     val message = remember {
         mutableStateOf("")
     }
-    // on below line creating variable
-    // for firebase auth and callback
-    val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
-    lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
+
     Surface(modifier = Modifier.fillMaxSize(),color= Color.White) {
         Column(
             horizontalAlignment = Alignment.Start,
             verticalArrangement = Arrangement.Top
         ) {
-            Button(
-                onClick = { navController.navigate(RYDXScreens.OpeningScreen.name) },
-                modifier = Modifier
-                    .padding(5.dp)
-                    .width(40.dp),
-                colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
-                contentPadding = PaddingValues(1.dp)
-            ) {
-                Text(
-                    text = "<", color = Color.Black,
-                    fontSize = 30.sp, fontWeight = FontWeight.Bold
-                )
-            }
+            BackButton(navController)
             Spacer(modifier = Modifier.height(25.dp))
             Logo()
             Spacer(modifier = Modifier.height(100.dp))
-            Row() {
-                TextField(
-                    value = stateIndication.value,
-                    // on below line specifying key board type as number.
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
-
-                    onValueChange = { stateIndication.value = it },
-
-                    placeholder = { Text(text = "C") },
-                    modifier = Modifier
-                        .padding(10.dp)
-                        .width(70.dp),
-                    textStyle = TextStyle(fontSize = 18.sp),
-                    singleLine = true,
-                )
-
-
-                TextField(
-                    value = phoneNumber.value,
-                    // on below line specifying key board type as number.
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-
-                    onValueChange = { phoneNumber.value = it },
-
-                    placeholder = { Text(text = "Enter your phone number") },
-                    modifier = Modifier
-                        .padding(10.dp)
-                        .fillMaxWidth(),
-                    textStyle = TextStyle(fontSize = 18.sp),
-                    singleLine = true,
-                )
-
-            }
-
+            PhoneNumberInput(stateIndication, phoneNumber)
         }
-        //----------------------
+        ToOtp(otp,navController,verificationID,mAuth,message)
+
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            verticalArrangement = Arrangement.Bottom
         ) {
-            Spacer(modifier = Modifier.height(80.dp))
-            TextField(
-                // on below line we are specifying
-                // value for our course duration text field.
-                value = otp.value,
-                //specifying key board on below line.
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                // on below line we are adding on
-                // value change for text field.
-                onValueChange = { otp.value = it },
+            Submit(phoneNumber, navController, stateIndication)
+            Spacer(modifier = Modifier.height(20.dp))
+            Register(navController)
+        }
+    }
+}
 
-                // on below line we are adding place holder
-                // as text as "Enter your course duration"
-                placeholder = { Text(text = "Enter your otp") },
+@Composable
+private fun Submit(
+    phoneNumber: MutableState<String>,
+    navController: NavController,
+    stateIndication: MutableState<String>
+) {
+    SubmitButton("Login",
+        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Black),
+        onClick = {
+            if (TextUtils.isEmpty(phoneNumber.value)) {
+                Toast.makeText(
+                    navController.context,
+                    "Please enter phone number..",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
 
-                // on below line we are adding modifier to it
-                // and adding padding to it and filling max width
-                modifier = Modifier
-                    .padding(5.dp)
-                    .fillMaxWidth(),
-
-                // on below line we are adding text style
-                // specifying color and font size to it.
-                textStyle = TextStyle(color = Color.Black, fontSize = 15.sp),
-
-                // on below line we are adding
-                // single line to it.
-                singleLine = true,
-            )
-
-            // adding spacer on below line.
-            Spacer(modifier = Modifier.height(10.dp))
-
-            // on below line creating button to add
-            // data to firebase firestore database.
-            Button(
-                onClick = {
-                    // on below line we are validating
-                    // user input parameters.
-                    if (TextUtils.isEmpty(otp.value)) {
-                        // displaying toast message on below line.
-                        Toast.makeText(navController.context, "Please enter otp..", Toast.LENGTH_SHORT).show()
-                    } else {
-                        // on below line generating phone credentials.
-                        val credential: PhoneAuthCredential = PhoneAuthProvider.getCredential(
-                            verificationID.value, otp.value
-                        )
-                        // on below line signing within credentials.
-                        signInWithPhoneAuthCredential(
-                            credential,
-                            mAuth,
-                            navController.context as Activity,
-                            navController.context,
-                            message
-                        )
-                    }
-                },
-                // on below line we are
-                // adding modifier to our button.
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(5.dp)
-            ) {
-                // on below line we are adding text for our button
-                Text(text = "Verify OTP", modifier = Modifier.padding(5.dp))
+                val number = stateIndication.value + phoneNumber.value
+                //navController.navigate(RYDXScreens.OTPVerificationScreen.name)
+                sendVerificationCode(number, mAuth, navController.context as Activity, callbacks)
             }
+        }
+    )
+}
 
-            // on below line adding spacer.
-            Spacer(modifier = Modifier.height(5.dp))
+@Composable
+private fun BackButton(navController: NavController) {
+    Button(
+        onClick = { navController.navigate(RYDXScreens.OpeningScreen.name) },
+        modifier = Modifier
+            .padding(5.dp)
+            .width(40.dp),
+        colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+        contentPadding = PaddingValues(1.dp)
+    ) {
+        Text(
+            text = "<", color = Color.Black,
+            fontSize = 30.sp, fontWeight = FontWeight.Bold
+        )
+    }
+}
 
-            Text(
-                // on below line displaying message for verification status.
-                text = message.value,
-                style = TextStyle(color = Color.Green, fontSize = 20.sp, fontWeight = FontWeight.Bold)
-            )
+@Composable
+private fun PhoneNumberInput(
+    stateIndication: MutableState<String>,
+    phoneNumber: MutableState<String>
+) {
+    Row {
+        TextField(
+            value = stateIndication.value,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            onValueChange = { stateIndication.value = it },
+            placeholder = { Text(text = "C") },
+            modifier = Modifier
+                .padding(10.dp)
+                .width(70.dp),
+            textStyle = TextStyle(fontSize = 18.sp),
+            singleLine = true,
+        )
+        TextField(
+            value = phoneNumber.value,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = { phoneNumber.value = it },
+            placeholder = { Text(text = "Enter your phone number") },
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth(),
+            textStyle = TextStyle(fontSize = 18.sp),
+            singleLine = true,
+        )
+    }
+}
 
-
-
+@Composable
+fun ToOtp(otp: MutableState<String>, navController: NavController,
+    verificationID: MutableState<String>, mAuth: FirebaseAuth,
+    message: MutableState<String>) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Spacer(modifier = Modifier.height(80.dp))
+        OtpFields(otp, navController, verificationID, mAuth, message)
         callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
             override fun onVerificationCompleted(p0: PhoneAuthCredential) {
                 // on below line updating message
@@ -212,37 +178,96 @@ fun  RYDXLoginScreen(navController: NavController = NavController(context = Loca
                 verificationID.value = verificationId
             }
         }
-        }
-        //-----------------
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Bottom
-        ) {
-            SubmitButton("Login",
-                colors = ButtonDefaults.buttonColors(backgroundColor = Color.Black),
-                onClick = {
-
-                        if (TextUtils.isEmpty(phoneNumber.value)) {
-                            Toast.makeText(navController.context,"Please enter phone number..", Toast.LENGTH_SHORT) .show()
-                        } else {
-
-                            val number = stateIndication.value + phoneNumber.value
-                            // on below line calling method to generate verification code.
-                            sendVerificationCode(number, mAuth, navController.context as Activity, callbacks)
-                        }
-
-                    //navController.navigate(RYDXScreens.OTPVerificationScreen.name)
-                }
-            )
-
-            Spacer(modifier = Modifier.height(20.dp))
-            register(navController)
-        }
     }
 }
 
 @Composable
-private fun register(navController: NavController) {
+private fun OtpFields(
+    otp: MutableState<String>,
+    navController: NavController,
+    verificationID: MutableState<String>,
+    mAuth: FirebaseAuth,
+    message: MutableState<String>
+) {
+    TextField(
+        // on below line we are specifying
+        // value for our course duration text field.
+        value = otp.value,
+        //specifying key board on below line.
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        // on below line we are adding on
+        // value change for text field.
+        onValueChange = { otp.value = it },
+
+        // on below line we are adding place holder
+        // as text as "Enter your course duration"
+        placeholder = { Text(text = "Enter your otp") },
+
+        // on below line we are adding modifier to it
+        // and adding padding to it and filling max width
+        modifier = Modifier
+            .padding(5.dp)
+            .fillMaxWidth(),
+
+        // on below line we are adding text style
+        // specifying color and font size to it.
+        textStyle = TextStyle(color = Color.Black, fontSize = 15.sp),
+
+        // on below line we are adding
+        // single line to it.
+        singleLine = true,
+    )
+
+    // adding spacer on below line.
+    Spacer(modifier = Modifier.height(10.dp))
+
+    // on below line creating button to add
+    // data to firebase firestore database.
+    Button(
+        onClick = {
+            // on below line we are validating
+            // user input parameters.
+            if (TextUtils.isEmpty(otp.value)) {
+                // displaying toast message on below line.
+                Toast.makeText(navController.context, "Please enter otp..", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                // on below line generating phone credentials.
+                val credential: PhoneAuthCredential = PhoneAuthProvider.getCredential(
+                    verificationID.value, otp.value
+                )
+                // on below line signing within credentials.
+                signInWithPhoneAuthCredential(
+                    credential,
+                    mAuth,
+                    navController.context as Activity,
+                    navController.context,
+                    message
+                )
+            }
+        },
+        // on below line we are
+        // adding modifier to our button.
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(5.dp)
+    ) {
+        // on below line we are adding text for our button
+        Text(text = "Verify OTP", modifier = Modifier.padding(5.dp))
+    }
+
+    // on below line adding spacer.
+    Spacer(modifier = Modifier.height(5.dp))
+
+    Text(
+        // on below line displaying message for verification status.
+        text = message.value,
+        style = TextStyle(color = Color.Green, fontSize = 20.sp, fontWeight = FontWeight.Bold)
+    )
+}
+
+@Composable
+private fun Register(navController: NavController) {
     Row(modifier = Modifier.padding(15.dp)) {
         Text(text = "Don't have an account?", color = Color.Black)
         Text("Register Now",
